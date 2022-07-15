785af0f5f26c614efbd3538788b8376680c64a34
dbranizan <dbranizan@gmail.com>
2022-07-15 18:36:49 -0300

    actualizacion 15 de julio

--- src/main/java/ar/com/codoacodo/controllers/ConsultarController.java
+++ src/main/java/ar/com/codoacodo/controllers/ConsultarController.java
@@ -1,10 +1,6 @@
 package ar.com.codoacodo.controllers;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ar.com.codoacodo.connection.AdministradorDeConexiones;
 import ar.com.codoacodo.daos.ProductoDAO;
 import ar.com.codoacodo.dto.Producto;
 
